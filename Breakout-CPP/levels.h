2d343d750843552b0d91171a61541f6f007681e6
Reeves Farrell <reevesfarrell13@gmail.com>
2026-01-23 16:16:20 -0500

    Add comments to clarify level system

--- Breakout-CPP/levels.h
+++ Breakout-CPP/levels.h
@@ -3,6 +3,10 @@
 
 #include "global.h"
 
+// 0 = Blank
+// 1 = Red
+// 2 = Yellow
+// 3 = Green
 constexpr int LEVEL_1[GAME_ROWS][GAME_COLS] = {
 	{0, 0, 0, 0, 0, 0, 0, 0},
 	{0, 1, 2, 3, 1, 2, 3, 0},
